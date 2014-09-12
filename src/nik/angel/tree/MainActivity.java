67905598d487b81f18b1f2e16150e8e1a803ed7a
sampath <sampath.skt@gmail.com>
2014-09-13 03:44:21 +0530

    is this first?

--- src/nik/angel/tree/MainActivity.java
+++ src/nik/angel/tree/MainActivity.java
@@ -2,7 +2,9 @@ package nik.angel.tree;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MotionEvent;
 
 public class MainActivity extends Activity {
 
@@ -12,7 +14,23 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
-
+    public boolean onTouchEvent(MotionEvent event) {
+        super.onTouchEvent(event);
+        switch (event.getAction()) {
+        case MotionEvent.ACTION_DOWN: {
+            //Log.e(TAG, " - DOWN -");
+            //Log.e(TAG, " getX: " + event.getX());
+            break;
+        }
+        case MotionEvent.ACTION_UP: {
+            //Log.e(TAG, " - UP -");
+            //Log.e(TAG, " getX: " + event.getX());
+            break;
+        }
+        }
+        return true;
+    }
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
