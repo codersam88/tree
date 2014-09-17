e4abbc93daf1545ca4d794658272c877469e1bdb
sampath <sampath.skt@gmail.com>
2014-09-17 18:02:13 +0530

    basic works

--- src/nik/angel/tree/MainActivity.java
+++ src/nik/angel/tree/MainActivity.java
@@ -1,20 +1,30 @@
 package nik.angel.tree;
 
+import java.util.UUID;
+
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener{
 	
+	private Button savBtn;
 	private Paper paper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //paper = (Paper)findViewById(R.id.paper);
+        paper = (Paper)findViewById(R.id.paper);
+        savBtn = (Button)findViewById(R.id.savBtn);
+        savBtn.setOnClickListener(this);
     }
 
     public boolean onTouchEvent(MotionEvent event) {
@@ -40,5 +50,17 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
+	
+
+	@Override
+	public void onClick(View v) {
+		paper.setDrawingCacheEnabled(true);
+		String imgSaved = MediaStore.Images.Media.insertImage(
+				getContentResolver(), paper.getDrawingCache(),
+				UUID.randomUUID().toString()+".png", "drawing");
+		paper.destroyDrawingCache();
+		
+	}
     
 }
